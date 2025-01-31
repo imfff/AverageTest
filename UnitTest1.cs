f3ff23f282172de61c47741871748f7b8ea5aa45
fuho <151814524+imfff@users.noreply.github.com>
2025-01-31 00:01:00 -0500

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using MyAverageLib;
+
+namespace MyAverageLibTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestAverageOfTwoNumbers()
+        {
+            double a = 10;
+            double b = 20;
+            double expected = 15;
+
+            double actual = AverageCalculator.Avg(a, b);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestAverageOfThreeNumbers()
+        {
+            double a = 10;
+            double b = 20;
+            double c = 30;
+            double expected = 20;
+
+            double actual = AverageCalculator.Avg(a, b, c);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
